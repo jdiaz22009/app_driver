ea625660cc2a670dc69bf027a491cf755e86e1ac
wihermes <wihermes1@gmail.com>
2019-02-08 11:52:07 -0500

    update addtional profile information

--- src/pages/driver/app/profile/profile-additional/profile-additional.ts
+++ src/pages/driver/app/profile/profile-additional/profile-additional.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core'
-import { IonicPage, NavController, NavParams } from 'ionic-angular'
+import { Component, ViewChild } from '@angular/core'
+import { IonicPage, NavController, NavParams, Content } from 'ionic-angular'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
-import { StorageDb } from '@providers/storageDb'
+import { DriverAuthProvider } from '@providers/api/driverAuth'
 import { CONFIG } from '@providers/config'
 
 @IonicPage()
@@ -11,17 +12,118 @@ import { CONFIG } from '@providers/config'
 })
 export class ProfileAdditionalDriverPage {
 
+  @ViewChild(Content) content: Content
+
+  profileForm: FormGroup
+  profileForm_0: FormGroup
+
+  profile: any
+  step_form: number = 0
+  step_images: any = [
+    './assets/imgs/step-1-2.png',
+    './assets/imgs/step-2-2.png'
+  ]
+  step_img: string = this.step_images[0]
+
 
   constructor(
     public navCtrl: NavController,
-    public db: StorageDb,
+    private formBuilder: FormBuilder,
+    public auth: DriverAuthProvider,
     public navParams: NavParams) {
 
-
+      this.setForms()
   }
 
   ionViewDidLoad(){
 
+  }
+
+  setForms(){
+    this.profileForm = this.formBuilder.group({
+      date: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required
+      ])],
+      place: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required
+      ])],
+      country: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required
+      ])],
+      state: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required
+      ])],
+      city: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required
+      ])],
+      address: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required
+      ])],
+      phone: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.required
+      ])]
+     })
+
+
+     this.profileForm_0 = this.formBuilder.group({
+      date: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required
+      ])],
+      place: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required
+      ])],
+      arl: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required
+      ])],
+      eps: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required
+      ])],
+      license_plate: ['', Validators.compose([
+        Validators.minLength(7),
+        Validators.required
+      ])],
+      license_category: ['', Validators.compose([
+        Validators.minLength(1),
+        Validators.required
+      ])],
+      license_expiration: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required
+      ])],
+      rh: ['', Validators.compose([
+        Validators.minLength(2),
+        Validators.required
+      ])],
+      gender: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required
+      ])]
+     })
+  }
+
+  save(){
+    if(this.step_form === 0){
+      this.step_form ++
+      this.step_img = this.step_images[1]
+      this.scrollToTop()
+    }else if(this.step_form === 1){
+
+    }
+  }
+
+  scrollToTop() {
+    this.content.scrollToTop()
   }
 
 }
--- src/pages/driver/app/profile/profile-basic/profile-basic.ts
+++ src/pages/driver/app/profile/profile-basic/profile-basic.ts
@@ -1,9 +1,9 @@
-import { Component } from "@angular/core"
-import { IonicPage, NavController, NavParams } from "ionic-angular"
+import { Component } from '@angular/core'
+import { IonicPage, NavController, NavParams } from 'ionic-angular'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 import { RegisterDriver } from '@models/registerDriver'
-import { DriverAuthProvider } from "@providers/api/driverAuth"
+import { DriverAuthProvider } from '@providers/api/driverAuth'
 
 @IonicPage()
 @Component({
@@ -18,7 +18,7 @@ export class ProfileBasicPage {
 
   email_validator = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  id_validator =  /^\d+$/
+  id_validator = /^\d+$/
 
   constructor(
     public navCtrl: NavController,
@@ -58,7 +58,6 @@ export class ProfileBasicPage {
         ])]
        })
 
-
       const user = this.navParams.get('profile')
       if(user != undefined){
         this.loadProfile(user)
@@ -66,13 +65,15 @@ export class ProfileBasicPage {
   }
 
   loadProfile(user){
-    this.profileForm.controls['id'].setValue(user.documento)
-    this.profileForm.controls['first_name'].setValue(user.primer_nombre)
-    this.profileForm.controls['second_name'].setValue(user.segundo_nombre)
-    this.profileForm.controls['first_lastname'].setValue(user.primer_apellido)
-    this.profileForm.controls['second_lastname'].setValue(user.segundo_apellido)
-    this.profileForm.controls['mobil'].setValue(user.celular)
-    this.profileForm.controls['email'].setValue(user.email)
+    const profileInput = this.profileForm.controls
+
+    profileInput['id'].setValue(user.documento)
+    profileInput['first_name'].setValue(user.primer_nombre)
+    profileInput['second_name'].setValue(user.segundo_nombre)
+    profileInput['first_lastname'].setValue(user.primer_apellido)
+    profileInput['second_lastname'].setValue(user.segundo_apellido)
+    profileInput['mobil'].setValue(user.celular)
+    profileInput['email'].setValue(user.email)
   }
 
   async update(){
