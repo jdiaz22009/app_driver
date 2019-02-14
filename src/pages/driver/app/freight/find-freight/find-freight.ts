bd6ef3d3ff7ae27d640a58355df0742c38f78b2c
wihermes <wihermes1@gmail.com>
2019-02-14 12:09:54 -0500

    update find freight

--- src/pages/driver/app/freight/find-freight/find-freight.ts
+++ src/pages/driver/app/freight/find-freight/find-freight.ts
@@ -1,7 +1,9 @@
+import { CONFIG } from '@providers/config';
 import { Component } from '@angular/core'
 import { IonicPage, NavController, NavParams } from 'ionic-angular'
 import 'rxjs/add/operator/map'
 
+import { StorageDb } from '@providers/storageDb'
 import { FreightProvider } from '@providers/api/freight'
 
 @IonicPage()
@@ -12,24 +14,61 @@ import { FreightProvider } from '@providers/api/freight'
 export class FindFreightDriverPage {
 
   offers: any = []
+  user_id: string
+
   constructor(
     public navCtrl: NavController,
     public apiFreight: FreightProvider,
+    public db: StorageDb,
     public navParams: NavParams) {
 
   }
 
   ionViewDidLoad(){
+
+  }
+
+  ionViewWillEnter(){
     this.getFreights()
   }
 
-  getFreights(){
+  async getUserId(){
+   const id = await this.db.getItem(CONFIG.localdb.USER_KEY).then(res =>{
+      return res.userId
+    })
+    return id
+  }
+
+  async getFreights(){
+    const userId = await this.getUserId()
+    console.log('UserId ' + userId)
+
     this.apiFreight.getOffert()
     .then(res =>{
       const data = res['data']
       // data.reverse()
-      data.sort((a, b) => new Date(b.fecha_creacion).getTime() - new Date(a.fecha_creacion).getTime())
-      this.offers = data
+
+      const array = []
+      data.forEach(e => {
+         const drivers = e['postulantes']
+         if(drivers.length > 0){
+            for(let i of drivers){
+              console.log(i)
+              if(i._id !== userId){
+                array.push(i)
+              }
+            }
+         }else{
+           array.push(e)
+         }
+      })
+
+      if(array.length > 0){
+        array.sort((a, b) => new Date(b.fecha_creacion).getTime() - new Date(a.fecha_creacion).getTime())
+      }
+
+      // console.log(JSON.stringify(data))
+      this.offers = array
 
     }).catch(e =>{
       console.error(e)
