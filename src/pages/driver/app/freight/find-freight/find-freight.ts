80bca65ef98c96de4c2566031f30cae236fc5f4c
wihermes <wihermes1@gmail.com>
2019-05-07 17:34:37 -0500

    add new template for offer shared

--- src/pages/driver/app/freight/find-freight/find-freight.ts
+++ src/pages/driver/app/freight/find-freight/find-freight.ts
@@ -93,15 +93,22 @@ export class FindFreightDriverPage {
   shared(freight){
     // console.log(JSON.stringify(freight))
     const initDate = new Date(freight.inicio).toLocaleDateString()
-    const message = `Oferta CargaYa
-                      Detalle: ${freight.Robservaciones}
-                      Flete: ${freight.flete} ,
-                      Fecha de inicio: ${initDate},
-                      Origen: ${freight.ciudad_origen} ,
-                      Desitno: ${freight.ciudad_destino} ,
-                      ID: ${freight._id}
-                      Ingresa a nuestra app y postúlate`
+    // const message = `Oferta CargaYa
+    //                   Detalle: ${freight.Robservaciones}
+    //                   Flete: ${freight.flete} ,
+    //                   Fecha de inicio: ${initDate},
+    //                   Origen: ${freight.ciudad_origen} ,
+    //                   Desitno: ${freight.ciudad_destino} ,
+    //                   ID: ${freight._id}
+    //                   Ingresa a nuestra app y postúlate`
     // const message = `Oferta CargaYa Detalle: ${freight.Robservaciones} Flete: $${freight.flete}, Fecha de inicio: ${initDate}, Origen: ${freight.ciudad_origen}, Desitno: ${freight.ciudad_destino}, ID: ${freight._id}, Ingresa a nuestra app y postúlate`
+
+    const message = `No DE PEDIDO: ${freight.pedido}.
+                     Ruta: ${freight.ciudad_origen}-${freight.ciudad_destino}.
+                     vehículo: ${freight.clase_vehiculo}-${freight.tipo_carroceria}.
+                     Peso: ${freight.peso_carga_max} TON.
+                     CONTACTO: ${freight.coordinador.primer_nombre} ${freight.coordinador.primer_apellido}, ${freight.coordinador.celular},
+                     Si quieres pago contra entrega, pregunta por PagaYa!!! http://tiny.cc/u09z5y`
 
     const subject  = 'Carga Disponible, postulate'
     const file = null
